08351b1049befcb4018dc99a083320bac2b31ddf
think-j <flame2080@naver.com>
2025-04-23 04:47:21 +0900

    Your commit message here

---
+++ lib/splash.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 4),
+      vsync: this,
+    );
+
+    _opacityAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.3),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _controller.forward();
+
+    // After 3 seconds go to Home Page
+    Timer(const Duration(seconds: 6), () {
+      Navigator.of(context).pushReplacementNamed('/home');
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: FadeTransition(
+          opacity: _opacityAnimation,
+          child: SlideTransition(
+            position: _slideAnimation,
+            child: Image.asset(
+              'assets/EOCFront.png',
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover, // or BoxFit.contain if you prefer
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
